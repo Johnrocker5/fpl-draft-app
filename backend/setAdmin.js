c74a684a10ae7987c7e1a67fc85c1c1bc2292d79
joshjohnstone <joshhjohnstone@gmail.com>
2025-08-11 19:28:22 +0200

    Give Admin privelleges to all for testing phase

--- backend/setAdmin.js
+++ backend/setAdmin.js
@@ -15,10 +15,25 @@ admin.initializeApp({
 });
 
 async function setAdminClaims() {
+  const uids = [
+    'O2Dim0NphuR5vacnpj4YKZFfTBd2', // Joshua
+    'bkNsCoQ4PAaqfkHvk371Foe4mAY2', // Marcus
+    '5kHk8rATvHZnP2AnH0AWddzreaJ3', // Jordan
+    'IHbdoSsvGiWDZ6iz5tWUwOXibWE2', // Matthew
+    'g3DLFGODHTVGEFIzckdQ2Pi87n83', // Antonie
+    'Z53R6llsyKfYoCogtUwuahmn8sJ2', // Leo
+    'wQxRQM0KRFVw5lgJPEkZasKLEZ32', // Kayle
+    'LiTo72DPI7gGYlj7quGAwOCRSux1', // Riccardo
+    'PFELQVetbkayGtcQW1I5dxFp0mA2', // Miko
+    'FyW01qVAZ2ecd7ZRbd1onGI46sw1'  // Crew
+  ];
+
   try {
-    const uid = 'O2Dim0NphuR5vacnpj4YKZFfTBd2';
-    await admin.auth().setCustomUserClaims(uid, { isAdmin: true });
-    console.log(`Admin claims set for UID: ${uid}`);
+    for (const uid of uids) {
+      await admin.auth().setCustomUserClaims(uid, { isAdmin: true });
+      console.log(`Admin claims set for UID: ${uid}`);
+    }
+    console.log('All admin claims set successfully');
     process.exit(0);
   } catch (err) {
     console.error('Error setting admin claims:', err);
